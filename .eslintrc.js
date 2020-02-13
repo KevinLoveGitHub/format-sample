1f66e1f36c0c63b75dbc0d9f890f729862053ae5
Kevin <616651408@qq.com>
2020-02-13 09:20:26 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/strongly-recommended',
+    'plugin:prettier/recommended',
+    'eslint:recommended'
+  ],
+  globals: {
+    __static: true
+  },
+  plugins: ['vue', 'html', 'prettier'],
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': 'error',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 每行最大属性
+    'vue/max-attributes-per-line': [
+      'error',
+      { singleline: 3, multiline: { max: 1, allowFirstLine: false } }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        }
+      }
+    ]
+  }
+}
--- README.md
+++ README.md
@@ -22,3 +22,11 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+[前端综合能力系列之EditorConfig](https://juejin.im/post/5ad05a085188257cc20db392).
+
+[使用ESlint & Prettier 美化Vue代码](https://nice.lovejade.cn/zh/article/beautify-vue-by-eslint-and-prettier.html#%E5%88%9D%E5%A7%8B%E5%8C%96-vue-%E9%A1%B9%E7%9B%AE%E6%8E%A8%E4%BB%8B)
+
+[格式化Vue代码及遇到问题](https://zhuanlan.zhihu.com/p/64627216)
+
+[Vue项目中ESlint规范](https://juejin.im/post/5d1c249ff265da1b5e730e99)
